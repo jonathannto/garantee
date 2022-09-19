bda4afc8cdc52cf2202771c9789785860c16bd46
Jonathan Nascimento <nascimento.jonathan@live.com>
2022-09-19 16:08:16 -0300

    configuração swagger

--- src/main/java/br/eng/jonathan/garantee/api/controller/CategoriaController.java
+++ src/main/java/br/eng/jonathan/garantee/api/controller/CategoriaController.java
@@ -9,19 +9,14 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.eng.jonathan.garantee.api.model.Categoria;
 import br.eng.jonathan.garantee.api.repository.CategoriaRepository;
 
 @RestController
-@RequestMapping("/categorias")
+@RequestMapping(value = "/categorias", produces = "application/json")
 public class CategoriaController {
 	
 	@Autowired
