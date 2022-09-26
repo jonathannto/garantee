ecb9f969d221d8ed818052285aaf3585fdfdfced
Jonathan Nascimento <nascimento.jonathan@live.com>
2022-09-26 10:49:45 -0300

    ajustes de testes

--- src/test/java/br/eng/jonathan/garantee/api/GaranteeApplicationTests.java
+++ src/test/java/br/eng/jonathan/garantee/api/GaranteeApplicationTests.java
@@ -1,6 +1,6 @@
 package br.eng.jonathan.garantee.api;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
