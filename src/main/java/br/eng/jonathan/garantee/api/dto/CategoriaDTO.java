1c18593b7907134cba2bb02a627612d28f0643f7
Jonathan Nascimento <nascimento.jonathan@live.com>
2022-10-05 10:58:02 -0300

    Merge melhoria de bean validation

--- src/main/java/br/eng/jonathan/garantee/api/dto/CategoriaDTO.java
+++ src/main/java/br/eng/jonathan/garantee/api/dto/CategoriaDTO.java
@@ -1,16 +1,6 @@
 package br.eng.jonathan.garantee.api.dto;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
-
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
 
 @Getter
 @Setter
